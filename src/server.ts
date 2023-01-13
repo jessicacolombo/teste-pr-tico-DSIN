49fb238974d62df9c9e22694436029b3e126bd8e
jessicacolombo <jessicacolombo384@gmail.com>
2023-01-12 23:44:40 -0300

    adjust port on server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import app from "./app";
 import AppDataSource from "./data-source";
+import "dotenv/config";
 
 (async () => {
   await AppDataSource.initialize().catch((err) => {
     console.error("Error during Data Source initialization", err);
   });
 
-  app.listen(3000, () => {
+  app.listen(process.env.PORT, () => {
     console.log("Servidor executando");
   });
 })();
